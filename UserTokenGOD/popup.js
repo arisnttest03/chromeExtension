85a88545c1836c27c6654cbf08f01276650b23aa
Anh Le Tuan 4 <anh.letuan4@nashtechglobal.com>
2023-08-25 11:12:13 +0700

    Adding btn

--- UserTokenGOD/popup.js
+++ UserTokenGOD/popup.js
@@ -8,7 +8,24 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const decodedUserToken = decodeURIComponent(userToken[0]);
 
     document.getElementById("userToken").textContent = decodedUserToken;
+    const copyButton = document.getElementById("btnCopy");
+    copyButton.addEventListener("click", function() {
+      const textToCopy = decodedUserToken;
+      const textarea = document.createElement("textarea");
+      textarea.value = textToCopy;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+
+      copyButton.textContent = "Copied!";
+      setTimeout(function() {
+        copyButton.textContent = "Click to copy token";
+      }, 2000);
+    });
   } else {
     document.getElementById("userToken").textContent = missingUserToken;
   }
+
+
 });
